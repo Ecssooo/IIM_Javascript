5a8c6cdfdc471a6681bcc608da860fc41a251c34
Ecssooo <147376957+Ecssooo@users.noreply.github.com>
2023-10-11 10:38:17 +0200

    Update classe.js

--- classe.js
+++ classe.js
@@ -16,23 +16,28 @@ class Pokemon {
         return degats;
     }
 }
-let pokemon1 = new Pokemon("salameche", 15, 5, 25);
-let pokemon2 = new Pokemon("bulbizarre", 10, 5, 35);
+let pokemon1 = new Pokemon("salameche", 15, 5, 25,0.5);
+let pokemon2 = new Pokemon("bulbizarre", 10, 5, 35,0.5);
 
 while (pokemon1.hp >= 0 && pokemon2.hp >= 0) {
-    luck = pokemon1.isLucky();
-    degats = pokemon1.attackPokemon(pokemon1, pokemon2);
+    let degats = 0
+    if(pokemon1.isLucky()){
+        degats = pokemon1.attackPokemon(pokemon1, pokemon2);
+    }
     console.log("vie de " + pokemon2.name + " " + pokemon2.hp + " degats pris : " + degats);
+    
     if (pokemon2.hp <= 0) {
         console.log(pokemon2.name + " n'a plus de vie");
         break;
     }
-    pokemon2.isLucky();
-    pokemon1.isLucky();
-    degats = pokemon2.attackPokemon(pokemon2, pokemon1);
+    luck = pokemon2.isLucky();
+    if(pokemon2.luck){
+        degats = pokemon2.attackPokemon(pokemon2, pokemon1);
     console.log("vie de " + pokemon1.name + " " + pokemon1.hp + " degats pris : " + degats);
+    
     if (pokemon1.hp <= 0) {
         console.log(pokemon1.name + " n'a plus de vie");
         break;
     }
+}
 }
